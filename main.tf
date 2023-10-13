66318db44813f0f17e4c04242dbf1b2dc048b2f1
Matt Trachier <matttrach@gmail.com>
2023-10-13 18:40:29 -0500

    don't download rpms (#6)
    Signed-off-by: matttrach <matttrach@gmail.com>

--- main.tf
+++ main.tf
@@ -1,35 +1,20 @@
 locals {
-  system          = var.system
-  rpm             = var.rpm
-  release         = var.release
-  latest          = (var.release == "latest" ? true : false)
-  release_tag     = (local.latest ? data.github_release.latest.release_tag : var.release)
-  sem             = regex("v([0-9]+)\\.([0-9]+)\\.([0-9]+)", local.release_tag) # extracts the semver from the release string
-  kube            = "${local.sem[0]}.${local.sem[1]}"                           # build the major.minor version of kubernetes
-  arch            = var.arch
-  os              = (local.rpm ? var.os : "")         # should be rhel, but should only be used when downloading RPMs
-  os_version      = (local.rpm ? var.os_version : "") # should only be used when downloading RPMs
-  install_url     = "https://raw.githubusercontent.com/rancher/rke2/master/install.sh"
-  rpm_os          = (local.os == "rhel" ? "centos" : local.os)
-  rpm_arch        = (local.arch == "amd64" ? "x86_64" : local.arch)
-  rpm_url         = "https://rpm.rancher.io/rke2/stable/${local.kube}/${local.rpm_os}/${local.os_version}"
-  rpm_release     = "${local.sem[0]}.${local.sem[1]}.${local.sem[2]}~rke2r1-0.el${local.os_version}.${local.arch}"
+  system      = var.system
+  arch        = var.arch
+  release     = var.release
+  latest      = (var.release == "latest" ? true : false)
+  install_url = "https://raw.githubusercontent.com/rancher/rke2/master/install.sh"
+  path        = abspath(var.path)
+
   selected_assets = (can(data.github_release.selected[0].assets) ? { for a in data.github_release.selected[0].assets : a.name => a.browser_download_url } : {})
   latest_assets   = (can(data.github_release.latest.assets) ? { for a in data.github_release.latest.assets : a.name => a.browser_download_url } : {})
   assets          = (local.latest ? local.latest_assets : local.selected_assets)
-  base_files = {
+  files = {
     "rke2-images.${local.system}-${local.arch}.tar.gz" = local.assets["rke2-images.${local.system}-${local.arch}.tar.gz"],
     "rke2.${local.system}-${local.arch}.tar.gz"        = local.assets["rke2.${local.system}-${local.arch}.tar.gz"],
     "sha256sum-${local.arch}.txt"                      = local.assets["sha256sum-${local.arch}.txt"],
     "install.sh"                                       = local.install_url,
   }
-  files = (local.rpm ? merge(local.base_files, {
-    "rke2-common.rpm"  = "${local.rpm_url}/${local.rpm_arch}/rke2-common-${local.rpm_release}.rpm",
-    "rke2-server.rpm"  = "${local.rpm_url}/${local.rpm_arch}/rke2-server-${local.rpm_release}.rpm",
-    "rke2-agent.rpm"   = "${local.rpm_url}/${local.rpm_arch}/rke2-agent-${local.rpm_release}.rpm",
-    "rke2-selinux.rpm" = "${local.rpm_url}/noarch/rke2-selinux-0.15-1.el${local.os_version}.noarch.rpm",
-  }) : local.base_files)
-  path = abspath(var.path)
 }
 
 data "github_release" "selected" {
