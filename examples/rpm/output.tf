831824eb48245d72fdb37dfd66574ddab0b0dcbb
matttrach <matttrach@gmail.com>
2023-10-13 17:57:23 -0500

    downloaded rpms can't be installed directly, this will need to be in a different module
    Signed-off-by: matttrach <matttrach@gmail.com>

--- examples/rpm/output.tf
+++
@@ -1,9 +0,0 @@
-output "tag" {
-  value = module.download_rpm.tag
-}
-output "path" {
-  value = module.download_rpm.path
-}
-output "files" {
-  value = module.download_rpm.files
-}
--- variables.tf
+++ variables.tf
@@ -3,7 +3,6 @@ variable "release" {
   description = <<-EOT
     The value of the git tag associated with the release to find.
     Specify "latest" to find the latest release (default).
-    When downloading RPMs, this must be a specific release, not "latest".
   EOT
   default     = "latest"
 }
@@ -23,41 +22,11 @@ variable "system" {
   EOT
   default     = "linux"
 }
-variable "os" {
-  type        = string
-  description = <<-EOT
-    The OS to download RPMs for.
-    This is only used for RPM downloads.
-    This is ignored when rpm is false.
-  EOT
-  default     = "rhel"
-}
-variable "os_version" {
-  type        = string
-  description = <<-EOT
-    The version of RHEL to download RPMs for.
-    This is only used for RPM downloads.
-    This is ignored when rpm is false.
-  EOT
-  default     = "8"
-}
-
 variable "path" {
   type        = string
   description = <<-EOT
     The path to download the files to.
-    If not specified, the files will be downloaded to a directory named "rke2" in the root of the module.
+    If not specified, the files will be downloaded to a directory named "rke2" in the root module.
   EOT
   default     = "./rke2"
-}
-variable "rpm" {
-  type        = bool
-  description = <<-EOT
-    Whether or not to download the RPMs.
-    Defaults to false.
-    This option requires that the system is linux (specifically RHEL based) and the architecture is amd64(x86_64).
-    This option requires the computer running terraform to have curl installed.
-    When using this option, the "release" variable must be set to a specific release, not "latest".
-  EOT
-  default     = false
 }
