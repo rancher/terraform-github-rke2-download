6ddc82f7ad3c2366ae86b7bffa5821b0f4a7d24c
matttrach <matt.trachier@suse.com>
2026-06-18 18:34:02 -0500

    fix: update flake
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-darwin" "aarch64-darwin" "x86_64-linux" "aarch64-linux" ]
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" "aarch64-linux" ]
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
@@ -16,10 +16,6 @@
             "selected" = "v0.70.0";
           };
           leftovers-prep = {
-            "x86_64-darwin" = {
-              "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-amd64";
-              "sha" = "sha256-HV12kHqB14lGDm1rh9nD1n7Jvw0rCnxmjC9gusw7jfo=";
-            };
             "aarch64-darwin" = {
               "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-arm64";
               "sha" = "sha256-Tw7G538RYZrwIauN7kI68u6aKS4d/0Efh+dirL/kzoM=";
@@ -28,9 +24,10 @@
               "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-linux-amd64";
               "sha" = "sha256-D2OPjLlV5xR3f+dVHu0ld6bQajD5Rv9GLCMCk9hXlu8=";
             };
+            # linux container running on darwin, actual arm linux isnt in the artifacts
             "aarch64-linux" = {
-              "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-linux-arm64";
-              "sha" = "sha256-qE1s3rUItGkP5o+yJ/1h20M0wXjN9r5R6s5wU+0i8oE=";
+              "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-arm64";
+              "sha" = "sha256-Tw7G538RYZrwIauN7kI68u6aKS4d/0Efh+dirL/kzoM=";
             };
           };
           leftovers = pkgs.stdenv.mkDerivation {
@@ -55,11 +52,6 @@
               "url" = "https://releases.hashicorp.com/terraform/${terraform-version.selected}/terraform_${terraform-version.selected}_darwin_arm64.zip";
               "sha" = "sha256-23wz6xpEa3OkQ+LFW1MoRfe3DNVhAL7EyW8Vz6tfUMs=";
               "checksum" = "db7c33eb1a446b73a443e2c55b532845f7b70cd56100bec4c96f15cfab5f50cb";
-            };
-            "x86_64-darwin" = {
-              "url" = "https://releases.hashicorp.com/terraform/${terraform-version.selected}/terraform_${terraform-version.selected}_darwin_amd64.zip";
-              "sha" = "sha256-jC54Ew5I7tNhlxH1sD8qN63G0d9M/Z1wZJ3kO9L1I0M=";
-              "checksum" = "841b8992f03f385c7bb9d4e5f7af8d9c20a4c2847a9649987820bb29124edc84";
             };
             "x86_64-linux" = {
               "url" = "https://releases.hashicorp.com/terraform/${terraform-version.selected}/terraform_${terraform-version.selected}_linux_amd64.zip";
@@ -90,48 +82,56 @@
             '';
           };
 
-          devPackages = [
-            leftovers
-            terraform
-          ] ++ (with pkgs; [
-            actionlint
-            bashInteractive
-            cmctl
-            cspell
-            curl
-            dig
-            gh
-            git
-            gitleaks
-            gnupg
-            go
-            gotestfmt
-            gotestsum
-            golangci-lint
-            kubernetes-helm
-            jq
-            kubectl
-            k8sgpt
-            less
-            openssh
-            openssl
-            pup
-            shellcheck
-            terraform-docs
-            tflint
-            tfsec
-            updatecli
-            vim
-            which
-            xan
-            yq-go
-          ]);
+        devPackages = [
+          # place our downloaded packages here
+          leftovers
+          terraform
+        ] ++ (with pkgs; [
+          # here are the packages from the nix repository
+          actionlint
+          age
+          awscli2
+          bashInteractive
+          cspell
+          curl
+          dig
+          eslint
+          gh
+          git
+          gitleaks
+          gnupg
+          go
+          golangci-lint
+          goreleaser
+          gotestfmt
+          gotestsum
+          jq
+          less
+          nodejs_26
+          openssh
+          openssl
+          shellcheck
+          tflint
+          tfsec
+          tree
+          trivy
+          updatecli
+          vim
+          which
+          yq-go
+        ]);
 
-          devShellPackage = pkgs.symlinkJoin {
-            name = "dev-shell-package";
-            paths = devPackages;
-          };
+        devShellPackage = pkgs.symlinkJoin {
+          name = "dev-shell-package";
 
+          # buildEnv properly handles combining all outputs (like bin, out, etc.)
+          paths = [
+            (pkgs.buildEnv {
+              name = "dev-shell-env";
+              paths = devPackages;
+            })
+          ];
+        };
         in
         {
           packages.default = devShellPackage;
